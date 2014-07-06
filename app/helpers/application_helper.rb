27552602afa8903e3f974e5cad5e9de02e75659a
saaransh25 <saaransh25@gmail.com>
2014-07-06 15:44:46 -0700

    Adding custom view helpers:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def fulltitle(pagetitle)
+		basetitle="Ruby on Rails Tutorial App"
+		if pagetitle.empty?
+			"#{basetitle}"
+		else
+			"#{basetitle} | #{pagetitle}"
+		end
+	end
 end

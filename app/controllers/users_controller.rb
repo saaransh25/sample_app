477e9be24c5c7b3230f22a3fabdc8facd0fe12f8
saaransh25 <saaransh25@gmail.com>
2014-07-07 00:15:32 -0700

    Sign-Up Page: Added new, create actions+handled validation errors on signup page+navigate to show page for each user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+  	@user=User.new
   end
+  def show
+  	@user=User.find(params[:id])
+  end
+  def create
+  	@user=User.new(user_params)
+  	if @user.save
+  		flash[:success]="Sign up was successful"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+  private
+  	def user_params
+  		params.required(:user).permit(:name,:email,:password,:password_confirmation)
+  	end
 end
